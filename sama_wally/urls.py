3ec417c01a339bfbf7407ade97d0ae96c3f41b5e
Mohammad Esmaeili <esmaeili@dev.bigdata.rightel.ir>
2024-01-30 09:14:07 +0330

    add: transaction urls

--- sama_wally/urls.py
+++ sama_wally/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wallet/', include('wallet.urls'))
+    path('wallets/', include('wallet.urls'))
 ]
--- wallet/urls.py
+++ wallet/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-
+    path('create-for-user/<int:user>', views.WalletCreateAPIView.as_view()),
+    path('user-wallets/<int:user>', views.WalletsListAPIView.as_view()),
+    path('<int:pk>', views.WalletRetrieveAPIView.as_view())
 ]
