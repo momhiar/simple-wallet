60c3e61a92f0daafa44430e22d79cc55c4aff632
Mohammad Esmaeili <esmaeili@dev.bigdata.rightel.ir>
2024-01-30 09:16:53 +0330

    add wallet transaction list apiiview

--- wallet/urls.py
+++ wallet/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('create-for-user/<int:user>', views.WalletCreateAPIView.as_view()),
     path('user-wallets/<int:user>', views.WalletsListAPIView.as_view()),
-    path('<int:pk>', views.WalletRetrieveAPIView.as_view())
+    path('<int:pk>', views.WalletRetrieveAPIView.as_view()),
+    path('wallet-transactions', views.TransactionListAPIView.as_view())
 ]
