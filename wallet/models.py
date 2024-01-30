69a9b23e604793436f0948e071e3c3a6f612e30a
Mohammad Esmaeili <esmaeili@dev.bigdata.rightel.ir>
2024-01-30 08:34:10 +0330

    add Wallet model

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# we skip some legally business needs for this task 
+# like masking Wallet ids like (xxxx-****-****-xxx) and ...
+class Wallet(models.Model):
+    wallet_name = models.CharField(max_length=75,blank=False, null=False)
+    owner = models.ForeignKey(User, db_index=True, on_delete=models.PROTECT)
+    balance = models.BigIntegerField(default=0)
+
